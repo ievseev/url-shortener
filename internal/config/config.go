ca2375733f8f061f59ab99dc911b233574403175
feliz <iaevseev@yandex.ru>
2026-01-17 17:21:37 +0700

    iter4: fix const

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,7 +16,7 @@ var (
 func init() {
 	// Регистрируем флаги при инициализации пакета
 	address = flag.String("a", "localhost:8080", "app address")
-	baseURL = flag.String("b", "http://localhost:8000/", "base url for response")
+	baseURL = flag.String("b", "http://localhost:8080/", "base url for response")
 }
 
 func Init() *AppConfig {
